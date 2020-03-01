de047fe10ac495d8b423cc0f5bdf7f963f835d44
jessiematias <jessiegmatias@gmail.com>
2020-02-29 21:39:53 -0400

    p5 project 1

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+  background('magenta');
+}
+
+function draw() {
+   fill(0, 225, 105,25);
+  ellipse(mouseX,mouseY,35,45);
+
+}
+
+function mousePressed(){
+  background('magenta');
+}
